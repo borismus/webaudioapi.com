3d1f908c6a2fda72398e627334c1c8fe2de68221
Boris Smus <boris@smus.com>
2021-11-02 13:07:52 -0700

    preview update

--- preview.py
+++ preview.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from livereload import Server, shell
 
 server = Server()
-server.watch('content', shell('lightning -o www'))
-server.serve(root='www')
+server.watch('content', shell('../lightning/lightning -o docs'))
+server.serve(root='docs')
